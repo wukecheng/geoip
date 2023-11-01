fe35f3f8b4f3f88f68de008b3d66ad6b624c4ef5
wukecheng <59279144+wukecheng@users.noreply.github.com>
2023-11-01 22:31:29 +0800

    1

--- asn.sh
+++ asn.sh
@@ -18,5 +18,6 @@ while IFS= read -r line; do
     curl -sL ${url} -o ./tmp/${filename}-${asn}.txt \
       -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
     jq --raw-output '.data.prefixes.v4.originating[]' ./tmp/${filename}-${asn}.txt | sort -u >>${file}
+    jq --raw-output '.data.prefixes.v6.originating[]' ./tmp/${filename}-${asn}.txt | sort -u >>${file}
   done
 done <${input}
